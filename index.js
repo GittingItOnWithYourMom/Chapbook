4c98460c01f08c9512a0248f0d3b1ca0b5e29ce4
GittingItOnWithYourMom <146044625+GittingItOnWithYourMom@users.noreply.github.com>
2024-11-22 21:31:16 +0000

    games 2

---
+++ index.js
@@ -0,0 +1,9 @@
+var games = document.getElementsByClassName('games')
+
+function start_game(game) {
+    for (var i = 0; i < games.length; i++) {
+        games[i].remove()
+    }
+    if (game == 1){
+    }
+}
