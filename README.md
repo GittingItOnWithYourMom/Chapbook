6fca3cc9ac7d379cfc8553950595f4c11c0aedbf
GittingItOnWithYourMom <146044625+GittingItOnWithYourMom@users.noreply.github.com>
2024-12-04 18:08:47 +0000

    stupid js doesnt work

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Chapbook
 My chapbook for Mr Osborn's class
+
+All code by Liam Neubauer
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function start_game(game) {
     for (var i = 0; i < games.length; i++) {
         games[i].remove()
     }
-    if (game == 1){
+    if (1 == 1){
+        window.open('carnival game/index.html')
     }
 }
