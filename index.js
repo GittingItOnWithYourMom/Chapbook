d8a1f345397966e6a93bd8c762e629d0dfd7fa90
GittingItOnWithYourMom <146044625+GittingItOnWithYourMom@users.noreply.github.com>
2024-12-05 19:58:40 +0000

    background

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var games = document.getElementsByClassName('games')
 
 function start_game(game) {
-    for (var i = 0; i < games.length; i++) {
-        games[i].remove()
-    }
-    if (1 == 1){
-        window.open('carnival game/index.html')
-    }
+    window.alert('yo')
+    //for (var i = 0; i < games.length; i++) {
+    //    games[i].remove()
+    //}
+    //if (1 == 1){
+    //    window.open('carnival game/index.html')
+    //}
 }
+
+document.getElementById('game1').addEventListener('click', start_game)
+document.getElementById('game2').addEventListener('click', start_game)
