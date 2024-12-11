7892d8829aa22444c7117735cb445ace50fbe502
GittingItOnWithYourMom <146044625+GittingItOnWithYourMom@users.noreply.github.com>
2024-12-11 19:41:16 +0000

    boxes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Chapbook
-My chapbook for Mr Osborn's class
+My chapbook for Mr Osborn's class.
 
-All code by Liam Neubauer
+All code by Liam Neubauer including the stuff that I stole.
--- index.js
+++ index.js
@@ -4,7 +4,24 @@ function start_game(game) {
    if (game == 1){
       window.open('/carnival game/index.html')
    }
-   for (var i = 0; i < 5; i++) {
-      games[i].remove()
-   }
+}
+
+function FadeInSection(props) {
+   const [isVisible, setVisible] = React.useState(true);
+   const domRef = React.useRef();
+   React.useEffect(() => {
+     const observer = new IntersectionObserver(entries => {
+       entries.forEach(entry => setVisible(entry.isIntersecting));
+     });
+     observer.observe(domRef.current);
+     return () => observer.unobserve(domRef.current);
+   }, []);
+   return (
+     <div
+       className={`fade-in-section ${isVisible ? 'is-visible' : ''}`}
+       ref={domRef}
+     >
+       {props.children}
+     </div>
+   );
 }
