be6507dae92fb9d31c8ae0fdfbe93ce68ca38b18
GittingItOnWithYourMom <146044625+GittingItOnWithYourMom@users.noreply.github.com>
2024-12-16 17:39:44 +0000

    fixed again

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var games = document.getElementsByClassName('games')
 
 function start_game(game) {
-  window.alert('yo')
   if (game == 1){
     window.open('/carnival game/index.html')
   }
@@ -14,24 +13,4 @@ function start_game(game) {
   if (game == 4){
     window.open('/paradox escape/index.html')
   }
-}
-
-function FadeInSection(props) {
-   const [isVisible, setVisible] = React.useState(true);
-   const domRef = React.useRef();
-   React.useEffect(() => {
-     const observer = new IntersectionObserver(entries => {
-       entries.forEach(entry => setVisible(entry.isIntersecting));
-     });
-     observer.observe(domRef.current);
-     return () => observer.unobserve(domRef.current);
-   }, []);
-   return (
-     <div
-       className={`fade-in-section ${isVisible ? 'is-visible' : ''}`}
-       ref={domRef}
-     >
-       {props.children}
-     </div>
-   );
 }
