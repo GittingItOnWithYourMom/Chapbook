4812ad3b8fd42fa1d6c3593a3ab3cfe0063d73dc
GittingItOnWithYourMom <146044625+GittingItOnWithYourMom@users.noreply.github.com>
2025-01-13 03:16:10 +0000

    i dont know what this is

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ var games = document.getElementsByClassName('games')
 
 function start_game(game) {
   if (game == 1){
-    window.open('/carnival game/index.html')
+    window.open('/web5/carnival game/index.html')
   }
   if (game == 2){
-    window.open('/universe escape/index.html')
+    window.open('/web5/universe escape/index.html')
   }
   if (game == 3){
-    window.open('/inside out/index.html')
+    window.open('/web5/inside out/index.html')
   }
   if (game == 4){
-    window.open('/paradox escape/index.html')
+    window.open('/web5/paradox escape/index.html')
   }
 }
